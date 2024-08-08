dea7c4ba39def1e1b51b7d97ef91da3314749219
Valeria Vagapova <lera.vagapova@gmail.com>
2024-08-08 19:00:01 +0300

    add database setup, wordpress cli, dependencies

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-exec mysqld_safe
+mysql_install_db
+
+exec mysqld
--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+# symlink php82 to php
+ln -s /usr/bin/php82 /usr/bin/php
+
+# install wp-cli
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+# start php-fpm
 exec php-fpm82 -F;
